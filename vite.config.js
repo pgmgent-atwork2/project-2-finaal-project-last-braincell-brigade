495dec50f7c5c275a711f5a901ec829a0514b211
Nelson-Mart <nelsonmartinel2@gmail.com>
2026-06-16 22:13:07 +0200

    Add build configuration with input paths for multiple views in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   server: {
@@ -9,4 +10,23 @@ export default defineConfig({
       },
     },
   },
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'src/views/login.html'),
+        home: resolve(__dirname, 'src/views/home.html'),
+        drinks: resolve(__dirname, 'src/views/drinks.html'),
+        planning: resolve(__dirname, 'src/views/planning.html'),
+        account: resolve(__dirname, 'src/views/myAccount.html'),
+        admin: resolve(__dirname, 'src/views/admin.html'),
+        drinksEditor: resolve(__dirname, 'src/views/drinks-editor.html'),
+        availability: resolve(__dirname, 'src/views/availability.html'),
+        users: resolve(__dirname, 'src/views/users.html'),
+        orders: resolve(__dirname, 'src/views/orders.html'),
+        matches: resolve(__dirname, 'src/views/matches.html'),
+      }
+    }
+  }
 });
